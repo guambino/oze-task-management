56cd87fb13a27a54ebf0c867a1c286bc306cf29a
guambino <ivan.guambe@quankee.co.za>
2025-02-13 21:38:39 +0200

    Repository Module done

---
+++ oze-task-management-repository/src/test/java/com/getoze/task/management/repository/AbstractRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.getoze.task.management.repository;
+
+import com.getoze.task.management.repository.config.RepositoryConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {RepositoryConfig.class})
+public abstract class AbstractRepositoryTest {
+}
