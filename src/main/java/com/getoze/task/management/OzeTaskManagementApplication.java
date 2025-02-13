b79bc1b4695326f99ca7804551d14e253b739e6e
guambino <ivan.guambe@quankee.co.za>
2025-02-13 15:57:08 +0200

    Removed unused folders

---
+++ src/main/java/com/getoze/task/management/OzeTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.getoze.task.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzeTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzeTaskManagementApplication.class, args);
+	}
+
+}
